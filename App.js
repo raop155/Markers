99f9e9604414a9b19f09deda4128582420bac57c
Ricardo Olarte <raop155@hotmail.com>
2020-12-31 16:04:15 -0500

    Add Map & Modal

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Modal, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <MapView style={styles.map} />
+      <Modal animationType='slide' transparent={true} visible={isOpen}>
+        <View style={styles.center}>
+          <View style={styles.modalView}>
+            <Text>😃</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
