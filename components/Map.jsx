656e956da17c9b9c01ac56546330aea714338574
Ricardo Olarte <raop155@hotmail.com>
2021-01-04 17:14:50 -0500

    feature(Map) Add device geolocation & loading state

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const Map = ({ onLongPress, points, showMarkers }) => {
+  const [location, setLocation] = useState(null);
+
+  const searchLocation = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+
+    if (status !== 'granted') {
+      return Alert.alert('You do not have the necessary permissions!!');
+    }
+    const location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+    setLocation(location);
+  };
+
+  useEffect(() => {
+    searchLocation();
+  }, []);
+
   return (
-    <MapView style={styles.map} onLongPress={onLongPress}>
-      {showMarkers &&
-        points.map((point) => {
-          return <Marker key={point.name} coordinate={point.coordinate} title={point.name} />;
-        })}
-    </MapView>
+    <>
+      {!location ? (
+        <MapView
+          style={styles.map}
+          onLongPress={onLongPress}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 1,
+            longitudeDelta: 1,
+          }}
+        >
+          {showMarkers &&
+            points.map((point) => {
+              return <Marker key={point.name} coordinate={point.coordinate} title={point.name} />;
+            })}
+        </MapView>
+      ) : (
+        <View style={styles.loading}>
+          <ActivityIndicator size='large' color='#0078d7' />
+          <Text style={styles.text}>Loading...</Text>
+        </View>
+      )}
+    </>
   );
 };
 
@@ -19,5 +56,14 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '80%',
+  },
+  loading: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '80%',
+  },
+  text: {
+    fontSize: 20,
   },
 });
